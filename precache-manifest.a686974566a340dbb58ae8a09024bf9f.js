6c7c8914dc50c80b2a6700b063e5abca3aa9dd8f
RishabMangal <rishabhmangal1@gmail.com>
2019-12-05 21:57:56 +0530

    Updates

---
+++ precache-manifest.a686974566a340dbb58ae8a09024bf9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519ecefe3392fd0e83f61135460505f9",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "05b24528d0f5dee5a674",
+    "url": "/Contact-Manager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "05b24528d0f5dee5a674",
+    "url": "/Contact-Manager/static/js/2.67154457.chunk.js"
+  },
+  {
+    "revision": "d5b73abd2c9c394d18d9",
+    "url": "/Contact-Manager/static/js/main.6c875df4.chunk.js"
+  },
+  {
+    "revision": "5f9879c0b7f1e9267e44",
+    "url": "/Contact-Manager/static/js/runtime-main.ae766fdf.js"
+  }
+]);
